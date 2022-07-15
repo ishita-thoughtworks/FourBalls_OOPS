35728d0236a7f5977ac54a317e3f81d92ba0cfaa
ishita-thoughtworks <108859064+ishita-thoughtworks@users.noreply.github.com>
2022-07-15 12:24:11 +0530

    Update FourBalls.java

--- src/main/java/org/ishita/FourBalls.java
+++ src/main/java/org/ishita/FourBalls.java
@@ -5,7 +5,7 @@ import processing.core.PApplet;
 public class FourBalls extends PApplet {
 
     public static final int HEIGHT = 640;
-    public static final int WIDTH = 740;
+    public static final int WIDTH = 480;
     static Circle circles[]=new Circle[4];
     public static void main(String[] args) {
         PApplet.main("org.ishita.FourBalls",args);
@@ -24,7 +24,7 @@ public class FourBalls extends PApplet {
     @Override
     public void draw() {
         for (int i=0;i<4;i++){
-            ellipse(circles[i].x_speed,circles[i].height,20,20);
+            ellipse(circles[i].x_speed,circles[i].height,10,10);
             circles[i].move_horizontal();
         }
     }
